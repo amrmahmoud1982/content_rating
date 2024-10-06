ed33f3d8f24c6281c33d8d7a37ae755a605e3172
Amr Emam <amrmahmoud1982@yahoo.com>
2024-10-06 11:01:22 +0300

    test amr

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -5,10 +5,34 @@ import './ContentRating.css';
 class ContentRating extends Component {
   constructor() {
     super();
+    this.state = {
+      likes : 0,
+      Dislikes : 0,
+      totalrating : 0,
+      handleLike: () => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1
+        }));
+      }
+
+    }
   }
   render() {
     return (
      <>
+     <div className='content-rating'>
+      <p> Amr Emam</p>
+      <div className='rating-buttons'>
+        <button className='like-button' onClick={this.state.handleLike}>
+          Like ({this.state.likes})
+        </button>
+        <button className='dislike-button'>
+          Dislike ({this.state.Dislikes})
+        </button>
+        <p>Total Rating:</p>
+
+      </div>
+     </div>
      <h1>Text Content Rating</h1>
      </>
     );
